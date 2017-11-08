4a04dfc8a1392ce29303eac5fb1b0b30f2a5bc0c
iTwicKz <twickes32@ufl.edu>
2017-11-07 22:35:25 -0500

    Complete projects.json

--- takashiw/src/App.js
+++ takashiw/src/App.js
@@ -3,78 +3,16 @@ import logo from './logo.svg'
 import Project from './components/ProjectView/ProjectView'
 import './App.css'
 import projects from './Projects.json'
-
-const rowHeight = 240
-const rowSpacing = 10
+import ProjectCollection from './components/ProjectCollection/ProjectCollection'
 
 class App extends Component {
 
-  getContainerStyle(isRightmost, size){
-    if(size > 1){
-      return { width: "66.66%" }
-    } else if (isRightmost){
-      return { float: "right"}
-    }
-  }
-
-  decrement(rowOccupied){
-    for(var i = 0; i < rowOccupied.length; i++){
-      rowOccupied[i]--
-    }
-    return rowOccupied
-  }
-
-  createGridSystem(){
-    var rows = []
-    var rowOccupied = [0, 0, 0]
-
-    for (var i=0; i < projects.length; i++) {
-      var projectData = projects[i]
-      var containerStyle = this.getContainerStyle(false, projectData.columns)
-
-      var isRightmost = false
-      var col = projectData.columns
-      var row = projectData.rows
-
-      var projectHeight =  row * (rowHeight + (row-1)*rowSpacing) + 'px'
-
-      for(var j = 0; j < col; j++){
-        for(var k = 0; k < rowOccupied.length; k++){
-            if(rowOccupied[k] == 0){
-              rowOccupied[k] = row
-              if(k == rowOccupied.length - 1){
-                isRightmost = true
-                containerStyle = this.getContainerStyle(true, projectData.columns)
-                rowOccupied = this.decrement(rowOccupied)
-              }
-              break;
-            }
-        }
-      }
-
-      rows.push(
-        <div className="container" style={containerStyle}>
-          <Project
-            title={projectData.title}
-            subtitle={projectData.subtitle}
-            backgroundColor={projectData.color}
-            image={projectData.thumbnail}
-            style={{height : projectHeight}}
-          />
-        </div>
-      )
-    }
-    return rows
-  }
-
   render() {
-    var rows = this.createGridSystem()
-
     return (
       <div className="App">
-        <div className="projectContainer">
-          {rows}
-        </div>
+        <h2>Hi there, my name is Takashi Wickes</h2>
+        <h2>and here is some of what I have built</h2>
+        <ProjectCollection />
       </div>
     )
   }
--- takashiw/src/components/ProjectView/ProjectView.js
+++ takashiw/src/components/ProjectView/ProjectView.js
@@ -4,13 +4,15 @@ import s from './style.css'
 class ProjectView extends Component {
   render() {
     return(
-        <div className="project" style={this.props.style}>
-          <img className="thumbnail" src={process.env.PUBLIC_URL + '/images/' + this.props.image}/>
-          <div className="titleContainer" style={{backgroundColor: this.props.backgroundColor}}>
-            <h1>{this.props.title}</h1>
-            <p>{this.props.subtitle}</p>
+        <a href={this.props.link} target="_blank">
+          <div className="project" style={this.props.style}>
+            <img className="thumbnail" src={process.env.PUBLIC_URL + '/images/' + this.props.image}/>
+            <div className="titleContainer" style={{backgroundColor: this.props.backgroundColor}}>
+              <h1>{this.props.title}</h1>
+              <p>{this.props.subtitle}</p>
+            </div>
           </div>
-        </div>
+        </a>
     )
   }
 }
